873d88ab40a6f96c8be13514ef7405aa926113e5
DevDuque <davihduque8@gmail.com>
2023-11-20 19:18:44 -0300

    Teste de conexão

--- src/main/java/org/SyncTask/Main.java
+++ src/main/java/org/SyncTask/Main.java
@@ -1,12 +1,51 @@
 package org.SyncTask;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
+
 public class Main {
+
     public static void main(String[] args) {
+        // Configurações do banco de dados
+        String url = "jdbc:mysql://localhost:3306/SyncTask";
+        String usuario = "2023954740";
+        String senha = "2023954740";
 
-        System.out.printf("Hello and welcome!");
+        try {
+            // Conectar ao banco de dados
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
 
-        for (int i = 1; i <= 5; i++) {
+            // Criar uma instrução SQL
+            Statement instrucao = conexao.createStatement();
 
-            System.out.println("i = " + i);
+            // Executar a consulta SQL
+            String sql = "SELECT UserID, Name, Username, CreatedAt FROM UserTable";
+            ResultSet resultado = instrucao.executeQuery(sql);
+
+            // Exibir os resultados
+            while (resultado.next()) {
+                UUID userId = UUID.fromString(resultado.getString("userId"));
+                String nome = resultado.getString("nome");
+                String username = resultado.getString("username");
+                String createdAt = resultado.getString("createdAt");
+
+                System.out.println("UserID: " + userId);
+                System.out.println("Nome: " + nome);
+                System.out.println("Username: " + username);
+                System.out.println("CreatedAt: " + createdAt);
+                System.out.println();
+            }
+
+            // Fechar recursos
+            resultado.close();
+            instrucao.close();
+            conexao.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
