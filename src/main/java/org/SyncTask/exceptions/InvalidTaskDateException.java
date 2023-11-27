a02413fff4d85b6357d1d048116b516bc2e63a0a
PedroHNO7 <pedrohnobholiver@gmail.com>
2023-11-27 20:54:02 -0300

    Subindo o InvalidTaskDateException.

---
+++ src/main/java/org/SyncTask/exceptions/InvalidTaskDateException.java
@@ -0,0 +1,8 @@
+package org.SyncTask.exceptions;
+
+public class InvalidTaskDateException extends Exception {
+
+    public InvalidTaskDateException(String message){
+        super(message);
+    }
+}
